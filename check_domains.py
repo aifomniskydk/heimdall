ae481ffd08e32034ae68fd8ba0589cff50c80f0b
aifomniskydk <121146274+aifomniskydk@users.noreply.github.com>
2026-02-27 09:40:31 +0100

    Add dns.resolver import to check_domains.py

--- check_domains.py
+++ check_domains.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as ET
 import tldextract
 from urllib.parse import urlparse
 import ipaddress
+import dns.resolver
 
 cf_ranges_cache = None  # global cache for Cloudflare IPs
 vercel_cidrs = ["76.76.21.0/24", "64.29.17.0/24", "66.33.60.0/24", "216.198.79.0/24"]
